ea42f52392faa502e33994c01899f8bb0dc11a12
Jeremy Ronquillo <eparis@atlassian.com>
2017-02-08 17:14:43 -0800

    Created the Project
    it says hello world <3

---
+++ Cache Money Project - COMP 129 Spring 2017/src/MainMenuScreen.java
@@ -0,0 +1,31 @@
+import javax.swing.JOptionPane;
+
+public class MainMenuScreen {
+	public MainMenuScreen(){
+		
+		// insert code here
+		
+	}
+	
+	public void createMenuWindow(){
+		while (true){
+			int x = JOptionPane.showConfirmDialog(null, "Hello, World! <3", "Cache Money Project", JOptionPane.OK_CANCEL_OPTION);
+			if (x == JOptionPane.OK_OPTION){
+				return;
+			}
+			else{
+				System.exit(0);
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args){
+		MainMenuScreen mms = new MainMenuScreen();
+		mms.createMenuWindow();
+		System.exit(0);
+	}
+	
+}
+
+
