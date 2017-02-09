f842c70164ae92ee2a66d04532e623262988c428
Mitchell Chang <Mitchell@10.15.161.25>
2017-02-09 13:21:07 -0800

    Main Menu Updated And Ready

--- Cache Money Project - COMP 129 Spring 2017/src/ScreenPack/MainMenuScreen.java
+++ Cache Money Project - COMP 129 Spring 2017/src/ScreenPack/MainMenuScreen.java
@@ -1,5 +1,9 @@
 package ScreenPack;
-import javax.swing.JOptionPane;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.*;
 
 public class MainMenuScreen {
 	public MainMenuScreen(){
@@ -9,22 +13,33 @@ public class MainMenuScreen {
 	}
 	
 	public void createMenuWindow(){
-		while (true){
-			int x = JOptionPane.showConfirmDialog(null, "Hello, World! <3", "Cache Money Project", JOptionPane.OK_CANCEL_OPTION);
-			if (x == JOptionPane.OK_OPTION){
-				return;
-			}
-			else{
-				System.exit(0);
-			}
-		}
-		
+			Font mainfont = new Font("Serif", Font.PLAIN, 14);
+			JPanel mainPanel = new JPanel(null);
+			mainPanel.setBackground(Color.green);
+			JFrame mainmenuframe = new JFrame("Main Menu");
+			mainmenuframe.add(mainPanel);
+			mainmenuframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			mainmenuframe.setVisible(true);
+			mainmenuframe.setSize(500,500);
+			int Width = mainmenuframe.getWidth();
+			int Height = mainmenuframe.getHeight();
+			JLabel HelloThere = new JLabel("I'm Hungry.");
+			HelloThere.setFont(mainfont);
+			HelloThere.setBounds(Width/2,100,Width/2,100);
+			mainPanel.add(HelloThere);
+			JButton GameButton = new JButton("Feed");
+			GameButton.setFont(mainfont);
+			GameButton.setBounds(100,200,100,100);
+			JButton ChatButton = new JButton("Me");
+			ChatButton.setFont(mainfont);
+			ChatButton.setBounds(300,300,100,100);
+			mainPanel.add(GameButton);
+			mainPanel.add(ChatButton);
 	}
 	
 	public static void main(String[] args){
 		MainMenuScreen mms = new MainMenuScreen();
 		mms.createMenuWindow();
-		System.exit(0);
 	}
 	
 }
