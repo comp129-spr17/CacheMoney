514c65e9a0ab16b6edce7a015ce0f3c2b383e2e1
Osvaldo <ojimenez@pacific.edu>
2017-02-15 15:20:16 -0800

    Working on Github issues

--- Cache Money Project - COMP 129 Spring 2017/src/GamePack/Piece.java
+++ Cache Money Project - COMP 129 Spring 2017/src/GamePack/Piece.java
@@ -3,6 +3,7 @@ package GamePack;
 public class Piece {
 	private String name;
 	//not sure what else to add to this class for now
+	//going to add a comment on line 6 to wreck this
 	public Piece(String n) {
 		name = n;
 	}
