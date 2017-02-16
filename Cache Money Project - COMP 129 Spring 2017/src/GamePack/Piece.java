ae1351b51b62262446155591d7686239b0d8c56b
Osvaldo <ojimenez@pacific.edu>
2017-02-15 16:11:52 -0800

    made some progress on piece (not really)

--- Cache Money Project - COMP 129 Spring 2017/src/GamePack/Piece.java
+++ Cache Money Project - COMP 129 Spring 2017/src/GamePack/Piece.java
@@ -3,6 +3,7 @@ package GamePack;
 public class Piece {
 	private String name;
 	//not sure what else to add to this class for now
+	//we've made some awesome progress on piece now!
 	public Piece(String n) {
 		name = n;
 	}
