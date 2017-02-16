10da5d263ccc15a655aca51dcc8f4919436b5d2e
Osvaldo <ojimenez@pacific.edu>
2017-02-15 16:10:08 -0800

    deleted stupid comment

--- Cache Money Project - COMP 129 Spring 2017/src/GamePack/Piece.java
+++ Cache Money Project - COMP 129 Spring 2017/src/GamePack/Piece.java
@@ -3,7 +3,6 @@ package GamePack;
 public class Piece {
 	private String name;
 	//not sure what else to add to this class for now
-	//going to add a comment on line 6 to wreck this
 	public Piece(String n) {
 		name = n;
 	}
