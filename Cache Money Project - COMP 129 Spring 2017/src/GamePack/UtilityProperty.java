0ce440372e3bd8e4042f789f9e901e767a6ac8b4
Michael Myers <m_myers6@u.pacific.edu>
2017-02-20 14:57:04 -0800

    Added a UtilityProperty class for the two utilities
    needs to be implemented

---
+++ Cache Money Project - COMP 129 Spring 2017/src/GamePack/UtilityProperty.java
@@ -0,0 +1,16 @@
+package GamePack;
+
+public class UtilityProperty extends Property {
+
+	public UtilityProperty(int cost) {
+		super(cost);
+		init();
+	}
+	
+	@Override
+	protected void init()
+	{
+		//This space charges the non-owners with 4*the dice amount rolled to land on this space
+			//If both Utilities are owned, it becomes 10* the dice amount rolled to land on this space
+	}
+}
