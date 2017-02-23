974ab9f56201d66bc7057ea6bcff636cbc82dfe5
Kadri Nizam <k_mohamadnizam@u.pacific.edu>
2017-02-22 17:21:38 -0800

    WildSpace bug fix: landOnSpace
    Landing on a wildSpace gives an array outOfBounds exception.

--- Cache Money Project - COMP 129 Spring 2017/src/GamePack/WildSpace.java
+++ Cache Money Project - COMP 129 Spring 2017/src/GamePack/WildSpace.java
@@ -14,7 +14,7 @@ public class WildSpace extends Space {
 	@Override
 	public int landOnSpace(Piece piece, int playerPosition) {
 		//TODO Functionality of a Chance/CommunityChest space needs to be implemented
-		super.receivePiece(piece, playerPosition);
+		//super.receivePiece(piece, playerPosition);
 		return playerPosition;
 	}
 	
