b1c930bfe2cae263554e35832edee33a32ea67b3
Jeremy Ronquillo <j_ronquillo@u.pacific.edu>
2017-02-23 20:14:30 -0800

    Added Waiting Room Pack and waiting room!

---
+++ Cache Money Project - COMP 129 Spring 2017/src/WaitingRoomPack/WRServer.java
@@ -0,0 +1,145 @@
+package WaitingRoomPack;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.BindException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import com.sun.jndi.cosnaming.IiopUrl.Address;
+
+public class WRServer {
+	private static ArrayList<WRElement> users;
+	private static ArrayList<PrintWriter> usersWriter;
+	private static ArrayList<WRThread> runningClients;
+	private static WRElement server;
+	private static int PORT_NUM = 1234;
+	private static ServerSocket listener;
+	private static WRClient hostClient;
+	
+	public WRServer() throws IOException{
+		Random rand = new Random();
+		PORT_NUM = rand.nextInt(8999 + 1000);
+		
+		System.out.println("Creating the server...");
+		while (true){
+			try{
+				listener = new ServerSocket(PORT_NUM);
+				break;
+			}
+			catch (BindException e){
+				PORT_NUM = rand.nextInt(8999 + 1000);
+			}
+		}
+		String ip = getIPAddress();
+		
+		
+		
+		System.out.println("Server successfully created!\n\n---------\n");		
+		System.out.println("Server IP Address: " + ip);
+		System.out.println("Server Port: " + listener.getLocalPort());
+		createHostClient(ip, listener.getLocalPort());
+		
+		
+        users = new ArrayList<>();
+        usersWriter = new ArrayList<>();
+        server = new WRElement("Server", "");
+        runningClients = new ArrayList<>();
+        closingServerAsking();
+        
+
+        while(true){
+        	try{
+            	WRThread aChatThread = new WRThread(listener.accept(), users, usersWriter,server, ip);
+            	runningClients.add(aChatThread);
+                aChatThread.start();
+        	}
+        	catch (IOException e){
+        		break;
+        	}
+        }
+	}
+	
+	
+	private String getIPAddress() throws SocketException, UnknownHostException {
+		Enumeration<NetworkInterface> n = NetworkInterface.getNetworkInterfaces();
+		for (int i = 0; n.hasMoreElements(); i++){
+			NetworkInterface e = n.nextElement();
+			Enumeration <InetAddress> a = e.getInetAddresses();
+			for (; a.hasMoreElements();){
+				InetAddress addr = a.nextElement();
+				if(addr.getHostAddress().indexOf("10.")==0){
+					System.out.println(" " + addr.getHostAddress());
+					return addr.getHostAddress();
+				}
+			}
+			//System.out.println(i);
+		}
+		return InetAddress.getLocalHost().getHostAddress();
+	}
+	
+	
+	private static void closeClientThreads(ArrayList<WRThread> lists){
+		for(WRThread list : lists){
+			list.disconnectServer();
+		}
+	}
+	private static void closingServerAsking(){
+		Timer aTimer = new Timer();
+		Scanner in = new Scanner(System.in);
+		aTimer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				System.out.println("Press enter or close client to close the \nserver and terminate program:\n");
+				in.nextLine();
+	        	closeServer();
+			}
+
+			
+		}, 0);
+		
+	}
+	
+	private static void closeServer() {
+    	System.out.println("Closing the server.....");
+		closeClientThreads(runningClients);
+    	try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+    	System.out.println("Finished cleaning up.");
+	}
+	
+	private static void createHostClient(String ip, int port){
+		Timer t = new Timer();
+		t.schedule(new TimerTask(){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				try {
+					hostClient = new WRClient(ip, port, true);
+					while (hostClient.getIsServerUp()){
+						//nothing
+					}
+					closeServer();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		}, 0);
+		
+	}
+}
---
+++ Cache Money Project - COMP 129 Spring 2017/src/WaitingRoomPack/WaitingRoom.java
@@ -0,0 +1,89 @@
+package WaitingRoomPack;
+
+import java.awt.Font;
+import java.awt.Panel;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import InterfacePack.Sounds;
+
+public class WaitingRoom extends JFrame {
+	
+	private final static int WINDOW_WIDTH = 700;
+	private final static int WINDOW_HEIGHT = 760;
+	private final static int MSGTYPEAREA_HEIGHT = 200;
+	private Panel panel;
+	private Object[] message;
+	private int numPeopleJoined;
+	private JTextArea msgDisplayArea;
+
+	
+	public WaitingRoom(boolean isHostClient){
+		init();
+		if (isHostClient){
+			hostWaitingRoom();
+		}
+		else{
+			clientWaitingRoom();
+		}
+		
+	}
+	
+	private void init(){
+		numPeopleJoined = 0;
+		
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		
+		panel = new Panel();
+		panel.setBounds(0,0,WINDOW_WIDTH, WINDOW_HEIGHT);
+		add(panel, null);
+		
+		msgDisplayArea = new JTextArea(30,55);
+		msgDisplayArea.setEditable(false);
+		msgDisplayArea.setBounds(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT - MSGTYPEAREA_HEIGHT);
+		msgDisplayArea.setFont(new Font("Serif",Font.BOLD,15));
+		panel.add(msgDisplayArea);
+		
+		this.setVisible(true);
+		panel.setVisible(true);
+		msgDisplayArea.setVisible(true);
+		
+	}
+	
+	private void hostWaitingRoom(){
+		msgDisplayArea.append("You are the host. Click start to begin playing, or wait for more players to join.\n");
+	}
+	
+	private void clientWaitingRoom(){
+		message = new Object[1];
+		message[0] = "You have joined a game! Please wait for the game to begin...";
+		System.out.println("You have joined a game! Please wait for the game to begin...");
+		
+		
+//		int chosen = JOptionPane.showConfirmDialog(null, message, "Client Waiting Room", JOptionPane.OK_CANCEL_OPTION);
+//		if (chosen == JOptionPane.CANCEL_OPTION){
+//			System.out.println("cancelled.");
+//		}
+	}
+	
+	public void receivedData(String msg){
+		if (msg.equals("joined")){
+			numPeopleJoined += 1;
+			Sounds.waitingRoomJoin.playSound();
+			msgDisplayArea.append("\nPlayer " + numPeopleJoined + " has joined!");
+		}
+		else{
+			System.out.println(msg);
+		}
+		
+	}
+	
+	
+}
