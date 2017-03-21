d207c84a502fea549f28d8d84f17a0d1a61b6594
Michael Myers <m_myers6@u.pacific.edu>
2017-03-21 13:55:40 -0700

    Jail Info now displayed somewhat. Still buggy. Not fully implemented yet

--- Cache Money Project - COMP 129 Spring 2017/src/ScreenPack/DicePanel.java
+++ Cache Money Project - COMP 129 Spring 2017/src/ScreenPack/DicePanel.java
@@ -147,6 +147,7 @@ public class DicePanel extends JPanel{
 		bPanel.add(propertyPanel);
 		mGamePanel = new MiniGamePanel(isSingle, this, bPanel,propertyPanel);
 		jailInfoScreen = new JailInfoPanel(this, isSingle, players, this, bPanel);
+		bPanel.add(jailInfoScreen);
 		this.board = board;
 	}
 	private void setDiceBackgroundColor() {
@@ -428,16 +429,16 @@ public class DicePanel extends JPanel{
 			propertyPanel.disableButtons();
 		}
 		else{
-			//if(!players[current].isInJail()) {
+			if(!players[current].isInJail()) {
 				rollButton.setVisible(true);
 				if (setDebugVisible){
 					overrideDiceRoll.setVisible(true);
 					toggleDoubles.setVisible(true);
 				}
-			//} else {
+			} else {
 				//TODO
-				//jailInfoScreen
-			//}
+				jailInfoScreen.executeSwitch(players[current], true);
+			}
 		}
 	}
 	public void actionForPropertyPurchase(String propertyName, int buyingPrice, int playerNum){
@@ -658,6 +659,8 @@ public class DicePanel extends JPanel{
 			}
 			else if (curSpaceName == "Chance" || curSpaceName == "Community Chest"){
 				Sounds.landedOnChanceOrCommunityChest.playSound();
+			} else if (curSpaceName == "Visiting Jail") {
+				
 			}
 		}
 		delayThread(600);
