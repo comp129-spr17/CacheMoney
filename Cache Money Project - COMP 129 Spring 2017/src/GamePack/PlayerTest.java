5029c480d679afac007e9ae9b8c01694241d1b03
DV <d_lim10@u.pacific.edu>
2017-03-28 22:06:44 -0700

    Added Some test cases

---
+++ Cache Money Project - COMP 129 Spring 2017/src/GamePack/PlayerTest.java
@@ -0,0 +1,55 @@
+package GamePack;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayerTest {
+	SizeRelated sizeRelated = SizeRelated.getInstance();
+	Player player0;
+	@Test
+	public void testGetTotalMonies() {
+		sizeRelated.setScreen_Width_Height(500, 500);
+		player0 = Player.getInstance(1);
+		assertTrue(player0.getTotalMonies()==1000000);
+		assertFalse(player0.getTotalMonies()==1000001);
+	}
+
+	@Test
+	public void testIsInJail() {
+		sizeRelated.setScreen_Width_Height(500, 500);
+		player0 = Player.getInstance(0);
+		assertFalse(player0.isInJail());
+		player0.setInJail(true);
+		assertTrue(player0.isInJail());
+	}
+
+	@Test
+	public void testGetPlayerNum() {
+		sizeRelated.setScreen_Width_Height(500, 500);
+		player0 = Player.getInstance(0);
+		assertFalse(player0.getPlayerNum() == 3);
+		assertTrue(player0.getPlayerNum() == 0);
+		
+	}
+
+	@Test
+	public void testGetPositionNumber() {
+		sizeRelated.setScreen_Width_Height(500, 500);
+		player0 = Player.getInstance(0);
+		assertTrue(player0.getPositionNumber() == 0);
+		player0.movePosition();
+		player0.movePosition();
+		assertTrue(player0.getPositionNumber() == 2);
+	}
+
+	@Test
+	public void testGetNumPropertiesOwned() {
+		sizeRelated.setScreen_Width_Height(500, 500);
+		player0 = Player.getInstance(0);
+		assertTrue(player0.getNumPropertiesOwned() == 0);
+		player0.purchaseProperty(new StandardProperty(500, "test", 500, 9));
+		assertTrue(player0.getNumPropertiesOwned() == 1);
+	}
+
+}
---
+++ Cache Money Project - COMP 129 Spring 2017/src/GamePack/PropertyTest.java
@@ -0,0 +1,75 @@
+package GamePack;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PropertyTest {
+	StandardProperty property = new StandardProperty(500, "Testing", 100, 9);
+
+	@Test
+	public void testGetBuyingPrice() {
+		assertTrue(property.getBuyingPrice() == 500);
+		assertFalse(property.getBuyingPrice() == 400);
+	}
+
+	@Test
+	public void testIsOwned() {
+		assertFalse(property.isOwned());
+		property.setOwned(true);
+		assertTrue(property.isOwned());
+	}
+
+	@Test
+	public void testGetName() {
+		assertTrue(property.getName().equals("Testing"));
+	}
+
+	@Test
+	public void testGetMortgageValue() {
+		assertTrue(property.getMortgageValue() == property.getBuyingPrice()/2);
+	}
+
+	@Test
+	public void testGetOwner() {
+		property.setOwner(1);
+		assertTrue(property.getOwner() == 1);
+		property.setOwner(0);
+		assertTrue(property.getOwner() == 0);
+	}
+
+	@Test
+	public void testIsMortgaged() {
+		assertFalse(property.isMortgaged());
+		property.setMortgagedTo(true);
+		assertTrue(property.isMortgaged());
+	}
+
+	@Test
+	public void testGetNumHouse() {
+		assertTrue(property.getNumHouse()==0);
+		property.incNumHouse();
+		property.incNumHouse();
+		assertTrue(property.getNumHouse()==2);
+		
+	}
+
+	@Test
+	public void testGetNumHotel() {
+		assertTrue(property.getNumHotel()==0);
+		property.incNumHouse();
+		property.incNumHouse();
+		assertFalse(property.getNumHotel()==1);
+		property.incNumHouse();
+		property.incNumHouse();
+		property.incNumHouse();
+		
+		assertTrue(property.getNumHotel()==1);
+	}
+
+	@Test
+	public void testGetPropertyFamilyIdentifier() {
+		assertTrue(property.getPropertyFamilyIdentifier() == 9);
+	}
+
+}
