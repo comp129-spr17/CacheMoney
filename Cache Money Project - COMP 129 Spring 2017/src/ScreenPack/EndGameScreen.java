54078c164e0a2357c6478ec417e5ed9a1e56c7e1
Michael Myers <2michaelmyers@gmail.com>
2017-04-13 11:28:22 -0700

    Started End Game Screen

---
+++ Cache Money Project - COMP 129 Spring 2017/src/ScreenPack/EndGameScreen.java
@@ -0,0 +1,18 @@
+package ScreenPack;
+
+public class EndGameScreen {
+	EndGameScreen() {
+		createLabels();
+		displayData();
+	}
+
+	private void displayData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void createLabels() {
+		// TODO Auto-generated method stub
+		
+	}
+}
